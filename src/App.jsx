7800fd2419eb068f2722538e4681e4d3e95f01ce
smuido <23smayaguido@gmail.com>
2026-04-11 22:23:36 -0700

    finished navbar and began spectator page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import Home from './pages/home';
+import Spectators from './pages/spectators';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <main>
+      <main className='main-content'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/competitors" element={<h1>Competitors</h1>} />
-          <Route path="/spectators" element={<h1>Spectators</h1>} />
+          <Route path="/spectators" element={<Spectators />} />
           <Route path="/past-events" element={<h1>Past Events</h1>} />
-          <Route path="/about-us" element={<h1>About Us</h1>} />
+          <Route path="/our-history" element={<h1>About Us</h1>} />
         </Routes>
       </main>
       <Footer />
---
+++ src/pages/spectators.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Spectators() {
+    return (
+        <div className="spectators-page">
+            <h1>Spectators</h1>
+            <div className='tickets_info'>
+                <h2>Tickets & Admission</h2>
+                    <p>Tickets for Mustang Ball can be purchased online or at the door. There are two tiers of spectator tickets.</p>
+                    <ul>
+                        <li><strong>General Spectator:</strong> Access to the venue and general seating.</li>
+                        <li><strong>Premium Spectator:</strong> Includes reserved front row seating during the evening session.</li>
+                    </ul>
+                    <p>Cal Poly students who present a valid PolyCard receive free admission to the event, but cannot occupy front row seats during the evening session.</p>
+            </div>
+            <h2>How to Enjoy the Competition</h2>
+                <p>Welcome to the Spectators page! Here you can find information about how to best enjoy the competition, where to buy tickets, and other details!</p>
+
+
+        </div>
+    );
+}
