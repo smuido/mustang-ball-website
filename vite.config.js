ea060d399b989f2a1e1dc3b4726ee364ced22275
Smaya Guido <23smayaguido@gmail.com>
2026-09-15 14:29:58 -0700

    fixed deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages serves this project at https://<user>.github.io/mustang-ball-websiite/,
+  // GitHub Pages serves this project at https://<user>.github.io/mustang-ball-website/,
   // not the domain root, so every built asset path needs this prefix.
-  base: '/mustang-ball-websiite/',
+  base: '/mustang-ball-website/',
 })
